367878fb6d6c267716f45622e5b088e7ddb42de2
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-08-27 21:12:36 -0300

    Comitando trabalho feito em aula

---
+++ EstruturaDeDadosJava/src/Teste/Aula02.java
@@ -0,0 +1,16 @@
+package Teste;
+
+import estruturaDadosVetor.Vetor;
+
+public class Aula02
+{
+    public static void main(String[] args)
+    {
+        Vetor vetor = new Vetor(2);
+        vetor.add("Olá");
+        vetor.add("Olá2;");
+    }
+
+
+
+}
